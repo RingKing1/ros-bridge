3abcda772ea505c0b26f31d21f24119b5d558590
ringking1 <wanghuan_1223@foxmail.com>
2025-04-08 15:05:35 +0800

    更改障碍物数量

--- carla_ros_bridge/launch/carla_ros_bridge_with_example_ego_vehicle.launch.py
+++ carla_ros_bridge/launch/carla_ros_bridge_with_example_ego_vehicle.launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
         ),
         launch.actions.DeclareLaunchArgument(
             name='town',
-            default_value='Town01_Opt'
+            default_value='Town01'
         ),
         launch.actions.DeclareLaunchArgument(
             name='passive',
